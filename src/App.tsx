64098fefb259732648fb622dfab872bacd6dba93
AngelDark0 <ksbeom39@gmail.com>
2023-11-28 21:52:48 -0500

    Se configuro la navegacion Funcional y el redireccionamiento de Paginas

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,16 @@
 import './App.css'
 import '@mantine/core/styles.css';
 
-import { MantineProvider } from '@mantine/core';
+import {  MantineProvider } from '@mantine/core';
 import NavBar from './components/AppBar/NavBar';
+import { Route, Routes } from 'react-router-dom';
+import { adminRoute, clientRoutes } from './utilities/routeApp';
+import Home from './components/viewClient/Home';
+import Login from './components/viewClient/Login';
+import Certificados from './components/viewAdmin/Certificados';
+import Curso from './components/viewAdmin/Curso';
+import Firma from './components/viewAdmin/Firma';
+import Plantilla from './components/viewAdmin/Plantilla';
 
 
 
@@ -16,7 +24,11 @@ function App() {
   return (
     <MantineProvider>
       <NavBar/>
-     
+      
+
+         
+          
+              
 
     </MantineProvider>
 
--- src/components/AppBar/NavBar.tsx
+++ src/components/AppBar/NavBar.tsx
@@ -1,5 +1,15 @@
 import { useDisclosure } from '@mantine/hooks';
 import { AppShell, Burger, Group,  UnstyledButton } from '@mantine/core';
+import { Link, NavLink, Route, Routes } from 'react-router-dom';
+import { adminRoute, clientRoutes } from '../../utilities/routeApp';
+import Home from '../viewClient/Home';
+import Login from '../viewClient/Login';
+import Certificados from '../viewAdmin/Certificados';
+import Curso from '../viewAdmin/Curso';
+import Firma from '../viewAdmin/Firma';
+import Plantilla from '../viewAdmin/Plantilla';
+import Contacto from '../viewClient/Contacto';
+
 
 
 export default function NavBar() {
@@ -22,19 +32,29 @@ export default function NavBar() {
             
             <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
                  <Group className='justify-between flex-1'>
+                  <Link to={clientRoutes.home} >
                     <Group>
                         <img src="/src/assets/logo.svg" alt="" className="w-20 h-18" />
                         <span className="text-lg font-bold font-sans ">Certificados FISEI</span>
          
                     </Group>
+                  </Link>
           
                 
 
             
            <Group className='font-semibold '>
+            <Link to={clientRoutes.home}>
             <UnstyledButton className=' hover:bg-gray-400 rounded-lg'>Inicio</UnstyledButton>
+            </Link>
+
+            <Link to={clientRoutes.administrador}>
             <UnstyledButton className=' hover:bg-gray-400 rounded-lg'>Administracion</UnstyledButton>
+            </Link>
+
+            <Link to={clientRoutes.contacto}> 
             <UnstyledButton className=' hover:bg-gray-400 rounded-lg'>Contactanos</UnstyledButton>
+            </Link>
           </Group>
           </Group>
         </Group>
@@ -42,13 +62,47 @@ export default function NavBar() {
       <AppShell.Navbar p="md" className='bg-gradient-to-bl from-gray-300 font-bold text-center'>
         Opciones
         <Group className='font-semibold grid gap-4 grid-cols-1 px-2 py-4 my-2'>
+            <Link to={adminRoute.generarCerti}>
+
             <UnstyledButton className=' hover:bg-gray-400 rounded-lg'>Generar Certificados</UnstyledButton>
+            </Link>
+
+            <Link to={adminRoute.cargarCurso}>
             <UnstyledButton className=' hover:bg-gray-400 rounded-lg'>Agregar Curso</UnstyledButton>
+            </Link>
+
+            <Link to={adminRoute.cargarFirma}>
             <UnstyledButton className=' hover:bg-gray-400 rounded-lg'>Agregar Firma</UnstyledButton>
+            </Link>
+
+            <Link to={adminRoute.cargarPalntilal}>
+
             <UnstyledButton className=' hover:bg-gray-400 rounded-lg'>Agregar Plantilla</UnstyledButton>
+            </Link>
         </Group>      
       </AppShell.Navbar>
-      <AppShell.Main>Esto son las vistas de los componentes</AppShell.Main>
+      <AppShell.Main>
+        
+      <Routes>
+
+<Route path={clientRoutes.default} element={<Home />} />
+<Route path={clientRoutes.home} element={<Home />} />
+<Route path={clientRoutes.administrador} element={<Login />} />
+<Route path={clientRoutes.contacto} element={<Contacto />} />
+
+
+<Route path={adminRoute.generarCerti} element={<Certificados />} />
+<Route path={adminRoute.cargarCurso} element={<Curso />} />
+
+  <Route path={adminRoute.cargarFirma} element={<Firma />} />
+  <Route path={adminRoute.cargarPalntilal} element={<Plantilla />} />
+
+
+
+
+</Routes>
+
+      </AppShell.Main>
       
       <AppShell.Footer className='bg-gradient-to-tr from-sky-700'>
         
