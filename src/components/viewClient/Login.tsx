a6c64e5b4322d24b14c1e8ccf672c9d485b37919
AngelDark0 <ksbeom39@gmail.com>
2023-12-07 10:15:51 -0500

    Diseño del modulo de loguin

--- src/components/viewClient/Login.tsx
+++ src/components/viewClient/Login.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button, Container, PasswordInput, TextInput } from '@mantine/core'
 
 
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <Container  size="xs" h={150} bg="var(--mantine-color-blue-light)" className="flex items-center justify-center h-screen" >
+    <form >
+      <TextInput withAsterisk
+      label="Usuario"
+      placeholder="Ingrese el usario">
+
+      </TextInput>
+      <PasswordInput
+      label="Contraseña"
+      placeholder="Ingrese la contraseña"
+      >
+      
+
+      </PasswordInput>
+
+      <Button fullWidth top={15} >
+        LogIn
+      </Button>
+
+    </form>
+   
+
+  </Container>
   )
 }
